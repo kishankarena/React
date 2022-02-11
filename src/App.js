d08fbcbcad0a31dbd1eae1bfedd94ca40c506f10
KKsimform <kishan.k@simformsolutions.com>
2022-02-11 08:53:44 +0530

    webpack installed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "App.css";
+import forest from "./images/forest.jpg"
+
+const App = () =>(
+    <div>
+        <h1>
+            Hello Word!!!
+        </h1>
+        <img src= {forest} alt="forest"/>
+        
+    </div>
+);
+
+export default App;
