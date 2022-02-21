dcd90a302858f81aa1c7d57c6af0b16118db0783
KKsimform <kishan.k@simformsolutions.com>
2022-02-21 18:49:42 +0530

    React state ,event and rendering list

--- react-practice/src/components/NewExpense/NewExpense.js
+++ react-practice/src/components/NewExpense/NewExpense.js
@@ -2,9 +2,17 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm"
 import './NewExpense.css'
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expeneseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+
+        props.onAddExpense(expeneseData);
+    }
     return <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
 };
 
