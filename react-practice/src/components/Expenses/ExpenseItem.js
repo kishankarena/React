9642b7849c341ec5a4d55cbb918959d55443f914
KKsimform <kishan.k@simformsolutions.com>
2022-02-22 16:54:23 +0530

    conditional rendering

--- react-practice/src/components/Expenses/ExpenseItem.js
+++ react-practice/src/components/Expenses/ExpenseItem.js
@@ -7,16 +7,17 @@ const ExpenseItem = (props) => {
 
 
    return (
+      <li>
+         <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+               <h2>{props.title}</h2>
+               <div className="expense-item__price">${props.amount}</div>
+            </div>
 
-      <Card className="expense-item">
-         <ExpenseDate date={props.date} />
-         <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
-         </div>
 
-
-      </Card>
+         </Card>
+      </li>
    );
 
 }
