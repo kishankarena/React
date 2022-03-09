a91d4ee568709b64ed74a66fda738aea3972dcec
KKsimform <kishan.k@simformsolutions.com>
2022-03-09 11:26:23 +0530

    redux

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { incNumber, decNumber } from './redux/actions/actions'
 
 
 function App() {
+  const myState = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="container">
       <h2>Increment/Decrement Counter</h2>
       <div>
-        <button className="action-counter">-</button>
-        <input className="counter" type="text" value="0" />
-        <button className="action-counter">+</button>
+        <button className="action-counter" onClick={() => dispatch(decNumber())}>-</button>
+        <input className="counter" type="text" value={myState} />
+        <button className="action-counter" onClick={() => dispatch(incNumber(5))}>+</button>
       </div>
     </div>
   );
