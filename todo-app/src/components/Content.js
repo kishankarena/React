cd7aa9a6334eef9ca9c00cab9f485bac2cf3fad6
KKsimform <kishan.k@simformsolutions.com>
2022-03-10 12:09:11 +0530

    changes commited

--- todo-app/src/components/Content.js
+++ todo-app/src/components/Content.js
@@ -1,19 +1,56 @@
-import React, { useState } from 'react'
-import List from './List'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import AddTask from "./addtask/AddTask";
 
+// getTitle function
+const getTitle = () => {
+  const list = localStorage.getItem("title");
+  if (list) {
+    return JSON.parse(localStorage.getItem("title"));
+  } else {
+    return [];
+  }
+};
+
+// content component
 const Content = (props) => {
-    return (
-        <ul className='app-content'>
-            {props.items.map((task) => {
-                return (
-                    <List
-                        key={task.id}
-                        storageKey={task.id}
-                        title={task.title} />
-                )
-            })}
-        </ul>
-    )
-}
+  const date = new Date();
+  const [tasks, setTasks] = useState(getTitle());
 
-export default Content
+  let taskDate = parseInt(localStorage.getItem("taskDate"));
+  let currDate = date.getDate();
+
+  useEffect(() => {
+    if (currDate !== taskDate) {
+      localStorage.clear();
+
+      console.log("clear localstorage");
+      if (localStorage.getItem("title") === null) {
+        setTasks(getTitle());
+      }
+    }
+  }, [currDate]);
+
+  // Add task to local storage
+  const addTaskHandler = (taskData) => {
+    setTasks((prevTaskData) => {
+      return [...prevTaskData, taskData];
+    });
+  };
+  useEffect(() => {
+    localStorage.setItem("title", JSON.stringify(tasks));
+  }, [tasks]);
+
+  return (
+    <>
+      <ul className="app-content">
+        {tasks.map((task) => {
+          return <List key={task.id} storageKey={task.id} title={task.title} />;
+        })}
+      </ul>
+      <AddTask onAddTask={addTaskHandler} />
+    </>
+  );
+};
+
+export default Content;
--- todo-app/src/components/addtask/AddTask.js
+++ todo-app/src/components/addtask/AddTask.js
@@ -1,29 +1,34 @@
-import React, { useState } from 'react'
-import TaskForm from './TaskForm';
+import React, { useState } from "react";
+import TaskForm from "./TaskForm";
 
 const AddTask = (props) => {
   const [isEditting, setIsEditting] = useState(false);
   const addDataHandler = (addedData) => {
     const taskData = {
       ...addedData,
-      id: Date.now()
+      id: Date.now(),
     };
     props.onAddTask(taskData);
-    setIsEditting(false)
-  }
+    setIsEditting(false);
+  };
   const startEditting = () => {
-    setIsEditting(true)
-  }
+    setIsEditting(true);
+  };
   const stopEditting = () => {
-    setIsEditting(false)
-  }
+    setIsEditting(false);
+  };
 
   return (
     <div>
-      {!isEditting &&
-        <button type="button" onClick={startEditting} className="add-button">+</button>}
-      {isEditting && <TaskForm onCancle={stopEditting} onAddData={addDataHandler} />}
+      {!isEditting && (
+        <button type="button" onClick={startEditting} className="add-button">
+          +
+        </button>
+      )}
+      {isEditting && (
+        <TaskForm onCancle={stopEditting} onAddData={addDataHandler} />
+      )}
     </div>
-  )
-}
-export default AddTask
+  );
+};
+export default AddTask;
